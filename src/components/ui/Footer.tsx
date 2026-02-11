65a98af65d7a89e6337afaf77cfd93f028418587
Razer <temporary00konto@gmail.com>
2026-02-11 15:26:16 +0530

    design: update logo to premium wrench SVG icon

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -11,8 +11,7 @@ export function Footer() {
                         <Link href="/" className="nav-logo">
                             <span className="logo-icon">
                                 <svg className="logo-svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M12 1L8.5 3H4.5V7L2.5 10.5V13.5L4.5 17V21H8.5L12 23L15.5 21H19.5V17L21.5 13.5V10.5L19.5 7V3H15.5L12 1ZM12 15.5C10.067 15.5 8.5 13.933 8.5 12C8.5 10.067 10.067 8.5 12 8.5C13.933 8.5 15.5 10.067 15.5 12C15.5 13.933 13.933 15.5 12 15.5Z" fill="currentColor" />
-                                    <path d="M12 10.5C11.1716 10.5 10.5 11.1716 10.5 12C10.5 12.8284 11.1716 13.5 12 13.5C12.8284 13.5 13.5 12.8284 13.5 12C13.5 11.1716 12.8284 10.5 12 10.5Z" fill="currentColor" />
+                                    <path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.77 3.77z" fill="currentColor" />
                                 </svg>
                             </span>
                             <span className="logo-text">Smart<span className="accent">Bike</span> Pro</span>
